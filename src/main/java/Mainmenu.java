1558122d47fbc5b7511ae0eec913e8f010b48adf
Serghinooo <sabarretoc@udistrital.edu.co>
2025-12-10 12:00:30 -0500

    Se agregan botones invertir promedio y ordenar y label

--- src/main/java/Mainmenu.java
+++ src/main/java/Mainmenu.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
+import java.util.Arrays ;
 
 public class Mainmenu {
     private JTabbedPane tabbedPane1;
-    private JTextField textField1;
-    private JButton button1;
+    private JTextField inputArray;
+    private JButton btnOrdenar;
+    private JButton btnInvertir;
+    private JButton btnPromedio;
+    private JLabel lblResultado;
+
 }
+
+
+
