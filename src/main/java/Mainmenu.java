7892bd2be4b93164ec67922d9130cd0cba336083
Serghinooo <sabarretoc@udistrital.edu.co>
2025-12-10 13:07:08 -0500

    Se terminó el diseño de la pestaña Arreglos en el Se configuró un panel con GridLayout de tres columnas y se colocaron los paneles de creación, operaciones y ordenamientos. Se añadieron textfields, textarea con scroll y todos los botones necesarios, renombrando cada componente con su field name para usarlo en el código. se ordenó la interfaz para que quedara limpia y funcional.

--- src/main/java/Mainmenu.java
+++ src/main/java/Mainmenu.java
@@ -1,9 +1,21 @@
 import javax.swing.*;
-import java.util.Arrays ;
 
 public class Mainmenu {
     private JTabbedPane tabbedPane1;
     private JPanel panel1;
+    private JTextField txtTamaño;
+    private JTextField txtElementos;
+    private JButton btnCrearArreglo;
+    private JButton btnLimpiar;
+    private JButton btnOrdenAsc;
+    private JButton btnSumar;
+    private JButton btnMinimo;
+    private JButton btnBuscar;
+    private JButton btnMaximo;
+    private JButton btnOrdenDesc;
+    private JTextArea txtResultado;
+    private JLabel t;
+    private JButton btnGuardar;
 
     private void createUIComponents() {
         panel1 = new JPanel();
